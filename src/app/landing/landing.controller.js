5f8a306042ef5fdd250b191085eb0d4af4901c9b
Nickson Dalimarta <nicksonjulian@utexas.edu>
2015-08-07 01:59:50 -0700

    make promise and intervalchange normal js var

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -12,40 +12,40 @@
   	vm.descriptions = ["Feature1","Feature2","Feature3"];
     vm.featurearr = feature_list;
 
-  	vm.intervalchange = function(){
+  	var intervalchange = function(){
   		if (vm.currfeature == 2)
   			vm.currfeature = 0;
   		else
   			vm.currfeature += 1;
   	}
 
-  	vm.promise = $interval(vm.intervalchange, 5000);  	
+  	var promise = $interval(intervalchange, 5000);  	
 
   	vm.changel = function(event){
   		event.preventDefault();
-  		$interval.cancel(vm.promise);
+  		$interval.cancel(promise);
   		if (vm.currfeature == 0)
   			vm.currfeature = 2;
   		else
 	  		vm.currfeature = vm.currfeature - 1;
-	  	vm.promise = $interval(vm.intervalchange, 5000);
+	  	promise = $interval(intervalchange, 5000);
   	};
 
   	vm.changer = function(event){
   		event.preventDefault();
-  		$interval.cancel(vm.promise);
+  		$interval.cancel(promise);
   		if (vm.currfeature == 2)
   			vm.currfeature = 0;
   		else
   			vm.currfeature += 1;
-  		vm.promise = $interval(vm.intervalchange, 5000);
+  		promise = $interval(intervalchange, 5000);
   	};
 
   	vm.changefeature = function(event, id){
   		event.preventDefault();
-  		$interval.cancel(vm.promise);
+  		$interval.cancel(promise);
   		vm.currfeature = id;
-  		vm.promise = $interval(vm.intervalchange, 5000);
+  		promise = $interval(intervalchange, 5000);
   	};
   }
 })();
