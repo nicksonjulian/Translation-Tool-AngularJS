e4e4c93bd73b06a823c043e90fce5586b0647618
Nickson Dalimarta <nicksonjulian@utexas.edu>
2015-08-07 01:26:26 -0700

    some fixed

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -3,31 +3,13 @@
 
   angular
     .module('translationToolAngularJs')
-    .controller('LandingController', ['$scope','$interval', LandingController]);
+    .controller('LandingController', LandingController);
 
   /** @ngInject */
-  function LandingController($scope, $interval) {
+  function LandingController($scope, $interval, feature_list) {
   	$scope.currfeature = 0;
   	$scope.descriptions = ["Feature1","Feature2","Feature3"];
-  	$scope.features =[
-  	{
-  		id:0,
-  		title: "FEATURE1",
-  		description: "feature1 desc",
-  		imgsrc: "app/landing/assets/dog.jpg"
-  	},
-  	{
-  		id:1,
-  		title: "FEATURE2",
-  		description: "feature2 desc",
-  		imgsrc: "app/landing/assets/cat.jpg"
-  	},
-  	{
-  		id:2,
-  		title: "FEATURE3",
-  		description: "feature3 desc",
-  		imgsrc: "app/landing/assets/bird.jpg"
-  	}];
+    $scope.features = feature_list;
 
   	$scope.intervalchange = function(){
   		if ($scope.currfeature == 2)
@@ -36,9 +18,7 @@
   			$scope.currfeature += 1;
   	}
 
-  	$scope.promise = $interval($scope.intervalchange, 5000);
-
-  	
+  	$scope.promise = $interval($scope.intervalchange, 5000);  	
 
   	$scope.changel = function(event){
   		event.preventDefault();
@@ -60,7 +40,7 @@
   		$scope.promise = $interval($scope.intervalchange, 5000);
   	};
 
-  	$scope.changea = function(event, id){
+  	$scope.changefeature = function(event, id){
   		event.preventDefault();
   		$interval.cancel($scope.promise);
   		$scope.currfeature = id;
