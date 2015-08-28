4c71ec23b64c1a55408aac5c78afb7f49ead05d7
Nickson Dalimarta <nicksonjulian@utexas.edu>
2015-08-27 20:44:28 -0700

    added working functional test

--- e2e/sign-in.po.js
+++ e2e/sign-in.po.js
@@ -1,19 +1,15 @@
 'use strict';
 
-var SignInPage  = function() {
-  this.usernameField = element(by.model('signInvm.username'));
+function SignInPage() {
+    this.usernameField = element(by.model('vm.username')),
+    this.passwordField = element(by.model('vm.password')),
+    this.signInButton = element(by.css('[ng-click="vm.logIn($event)"]')),
+    this.failMessage = element(by.binding('vm.failmessage')),
+    this.failLogin = element(by.css('[ng-show="vm.faillogin"].errormessage')),
+    this.signIn = function(username, password) {
+        this.usernameField.sendKeys(username);
+        this.passwordField.sendKeys(password);
+    }
+}
 
-  this.passwordField = element(by.model('signInvm.password'));
-
-  this.signInButton = element(by.css('.md-button'));
-
-  this.failMessage = element(by.binding('vm.failmessage')).getText();
-
-
-  this.signIn = function(username, password) {  
-    this.usernameField.sendKeys(username);
-    this.passwordField.sendKeys(password);
-    this.signInButton.click();}
-};
-
-module.exports = new SignInPage();
+module.exports = SignInPage;
